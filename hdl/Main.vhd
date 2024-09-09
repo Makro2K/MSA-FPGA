099fb775201815f41a8f968e9d42d16dc48ea04f
Maciej Kromer <132134219+Makro2K@users.noreply.github.com>
2024-09-09 15:49:42 +0200

    Update Main.vhd

--- hdl/Main.vhd
+++ hdl/Main.vhd
@@ -42,7 +42,8 @@ begin
     m <= std_logic_vector(TO_UNSIGNED(63,32)) when data_m_i > 63 else data_m_i;
     process(clk_i,rst_i)
     variable unsigned_long: unsigned (31 downto 0) := TO_UNSIGNED(1, 32);
-    variable tmp_m: std_logic_vector (31 downto 0);
+    variable tmp_data:      std_logic_vector (31 downto 0);
+    variable i:             integer := 0;
     begin
         if rising_edge(clk_i) then
             if (rst_i = '0' or control_i = 0) then
@@ -56,6 +57,7 @@ begin
                 diff <= (others => '0');
                 b <= (others => (others => '0'));
                 stage <= (others => '0');
+                data_o <= (others => '0');
             else
                 case stage is
                 when "000" =>
@@ -65,18 +67,50 @@ begin
                     end if;
                 when "001" =>
                     status_o <= std_logic_vector(TO_UNSIGNED(1,32));
-                    tmp_m := m;
-                    diff <= tmp_m;
-                    k <= tmp_m(7 downto 0);   
-                    for i in 0 to 1000 loop
-                        exit when i = tmp_m;
-                        
-                        status_o <= std_logic_vector(TO_UNSIGNED(2,32));
-                        b(i) <= (b(i) or std_logic_vector(shift_left(unsigned_long,i)));
-                    end loop;
-                    unsigned_long := shift_left(unsigned_long,TO_INTEGER(unsigned(tmp_m)));
-                    VP <= std_logic_vector(shift_left(unsigned_long,TO_INTEGER(unsigned(tmp_m)))) - 1;
+                    tmp_data := m;
+                    diff <= tmp_data;
+                    k <= tmp_data(7 downto 0);                       
+                    VP <= std_logic_vector(shift_left(unsigned_long,TO_INTEGER(unsigned(tmp_data)))) - 1;
                     VN <= (others => '0');
+                    stage <= "010";
+                when "010" =>
+                    status_o <= std_logic_vector(TO_UNSIGNED(2,32));
+                    if(control_i = 2) then
+                        status_o <= std_logic_vector(TO_UNSIGNED(1,32));
+                        tmp_data := data_p_i;
+                        b(TO_INTEGER(unsigned(tmp_data))) <= (b(TO_INTEGER(unsigned(tmp_data))) or std_logic_vector(shift_left(unsigned_long,i)));
+                        i := i + 1;
+                    elsif (control_i = 3) then
+                        stage <= "011";
+                        status_o <= std_logic_vector(TO_UNSIGNED(1,32));
+                        i := 0;
+                        tmp_data := tmp_data - 1;
+                        MASK <= std_logic_vector(shift_left(unsigned_long,TO_INTEGER(unsigned(tmp_data))));
+                    end if;
+                when "011" =>
+                    status_o <= std_logic_vector(TO_UNSIGNED(3,32));
+                    if (control_i = 3) then
+                        status_o <= std_logic_vector(TO_UNSIGNED(1,32));
+                        tmp_data := data_t_i;
+                        X <= b(TO_INTEGER(unsigned(tmp_data))) or VN;
+                        D0 <= X or (VP xor (VP + (X and VP)));
+                        HN <= VP and D0;
+                        HP <= VN or (not(VP or D0));
+                        X <= std_logic_vector(shift_left(unsigned(HP), 1));
+                        VN <= X and D0;
+                        VP <= (std_logic_vector(shift_left(unsigned(HN), 1))) or (not(X or D0));
+                        if (HP and MASK) = 1 then
+                            diff <= diff + 1;
+                        end if;
+                        if (HN and MASK) = 1 then
+                            diff <= diff - 1;
+                        end if;
+                        if(diff < k) then
+                            k <= diff;
+                        end if;
+                        data_o <= k;
+                        status_o <= std_logic_vector(TO_UNSIGNED(3,32));
+                    end if; 
                 when others =>
                 end case; 
             end if;
